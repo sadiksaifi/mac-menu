63eb4578a863c00eaf1e4293205944ab755ffdb2
Sadik Saifi <mail@sadiksaifi.dev>
2025-11-24 06:07:10 +0530

    chore: automate homebrew formula updates

--- Formula/mac-menu.rb
+++ Formula/mac-menu.rb
@@ -1,8 +1,8 @@
 class MacMenu < Formula
   desc "A command line tool for Mac"
   homepage "https://github.com/sadiksaifi/mac-menu"
-  url "https://github.com/sadiksaifi/mac-menu/releases/download/v0.0.1/mac-menu.tar.gz" # Placeholder
-  sha256 "REPLACE_WITH_SHA256"
+  url "https://github.com/sadiksaifi/mac-menu/releases/download/v0.0.3/mac-menu.tar.gz"
+  sha256 "20722f8e888df5e6d8316c77c0af56e770a9ef7193a86241b80bf5689c61d688"
   license "MIT"
 
   def install
